954d3de0cfbf40a6bc70dfa4508f46164a90be5f
reshmyunni <reshmyunni1778@gmail.com>
2022-12-15 14:53:54 +0530

    first commit

---
+++ SampleUI.framework/Headers/SampleUI.h
@@ -0,0 +1,18 @@
+//
+//  SampleUI.h
+//  SampleUI
+//
+//  Created by Reshmy Manuraj on 15/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleUI.
+FOUNDATION_EXPORT double SampleUIVersionNumber;
+
+//! Project version string for SampleUI.
+FOUNDATION_EXPORT const unsigned char SampleUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleUI/PublicHeader.h>
+
+
